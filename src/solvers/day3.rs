6d197e33d5cce2ac95ba626478e111c9e2c41fb6
Davide Pedron <dpedron@pubstack.io>
2022-12-15 10:38:03 +0100

    Solve day 3 part B

--- src/solvers/day3.rs
+++ src/solvers/day3.rs
@@ -2,18 +2,51 @@ use std::collections::HashSet;
 use crate::Part;
 
 pub fn solve(part: Part, lines: Vec<String>) -> String {
-    let scores: Vec<u32> = lines.into_iter()
-        .map(split_in_compartments)
-        .map(found_common_element)
-        .map(convert_to_priority)
-        .collect();
+    let scores: Vec<u32> = match part {
+        Part::A => lines.into_iter()
+            .map(split_in_compartments)
+            .map(found_common_element)
+            .map(convert_to_priority)
+            .collect(),
+        Part::B => found_badges(lines)
+            .into_iter()
+            .map(convert_to_priority)
+            .collect()
+    };
 
     let result: u32 = scores.iter().sum();
     String::from(format!("{}", result))
 }
 
+fn found_badges(rucksacks: Vec<String>) -> Vec<char> {
+    let mut badges: Vec<char> = Vec::new();
+    let mut current_badge: Vec<char> = Vec::new();
+    for rucksack in rucksacks {
+        if current_badge.is_empty() {
+            rucksack.chars()
+                .for_each(|e| if !current_badge.contains(&e) {
+                    current_badge.push(e);
+                });
+        } else {
+            let prev_elements = current_badge.clone();
+            current_badge = Vec::new();
+            rucksack
+                .chars()
+                .filter(|e| prev_elements.contains(e))
+                .for_each(|e| if !current_badge.contains(&e) {
+                    current_badge.push(e);
+                })
+        }
+        if current_badge.len() == 1 {
+            badges.push(*current_badge.first().unwrap());
+            current_badge = Vec::new();
+        }
+    }
+    badges
+}
+
 fn split_in_compartments(rucksacks_content: String) -> (String, String) {
-    let split_index = rucksacks_content.clone().len() / 2;
+    let split_index = rucksacks_content.len() / 2;
     let x = rucksacks_content.split_at(split_index);
     (x.0.to_string(), x.1.to_string())
 }
@@ -21,7 +54,7 @@ fn split_in_compartments(rucksacks_content: String) -> (String, String) {
 fn found_common_element(compartment_elements: (String, String)) -> char {
     let set: HashSet<char> = compartment_elements.0.chars().collect();
     let commons: Vec<char> = compartment_elements.1.chars()
-        .filter(|c| set.contains(&c))
+        .filter(|c| set.contains(c))
         .collect();
     commons.first().unwrap().clone()
 }
@@ -38,7 +71,7 @@ fn convert_to_priority(element: char) -> u32 {
 #[cfg(test)]
 mod tests {
     use crate::Part;
-    use crate::solvers::day3::{found_common_element, solve, split_in_compartments};
+    use crate::solvers::day3::{found_badges, found_common_element, solve, split_in_compartments};
 
     #[test]
     fn solve_example_part_a() {
@@ -55,6 +88,20 @@ mod tests {
     }
 
     #[test]
+    fn solve_example_part_b() {
+        let lines = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp".to_string(),
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string(),
+            "PmmdzqPrVvPwwTWBwg".to_string(),
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".to_string(),
+            "ttgJtRGJQctTZtZT".to_string(),
+            "CrZsJsPPZsGzwwsLwLmpwMDw".to_string(),
+        ];
+        let result = solve(Part::B, lines);
+        assert_eq!(result, "70");
+    }
+
+    #[test]
     fn it_split_a_string_in_half() {
         let result = split_in_compartments(String::from("AAAABBBB"));
 
@@ -68,4 +115,28 @@ mod tests {
         assert_eq!(result, 'x');
     }
 
+    #[test]
+    fn it_found_badges_in_single_group() {
+        let lines = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp".to_string(),
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string(),
+            "PmmdzqPrVvPwwTWBwg".to_string(),
+        ];
+        let result = found_badges(lines);
+        assert_eq!(result, vec!['r']);
+    }
+
+    #[test]
+    fn it_found_badges() {
+        let lines = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp".to_string(),
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string(),
+            "PmmdzqPrVvPwwTWBwg".to_string(),
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".to_string(),
+            "ttgJtRGJQctTZtZT".to_string(),
+            "CrZsJsPPZsGzwwsLwLmpwMDw".to_string(),
+        ];
+        let result = found_badges(lines);
+        assert_eq!(result, vec!['r', 'Z']);
+    }
 }
